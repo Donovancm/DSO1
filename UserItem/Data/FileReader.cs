bddd2b82bb012cf1e318fcc96d24b327b9d9140a
Donovancm <donovan.dcm@gmail.com>
2019-02-27 14:33:02 +0100

    own classes for filereader

---
+++ UserItem/Data/FileReader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserItem.Data
+{
+    class FileReader
+    {
+        public FileReader()
+        {
+
+        }
+        public Dictionary<double, double[,]> GetData(double[,] data)
+        {
+            var dictionary = new Dictionary<double, double[,]>();
+            int rowLength = data.GetLength(0);
+            int colLenght = data.GetLength(1);
+            bool checkeNewRow = false;
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                double key = -1;
+                double productvalue = -1;
+                double userRating = -1;
+                checkeNewRow = false;
+
+                for (int j = 0; j < colLenght; j++)
+                {
+                    Console.WriteLine(data[i, j]);
+                    if (!checkeNewRow)
+                    {
+                        key = data[i, j];
+                        checkeNewRow = true;
+
+                    }
+
+                    else if (j == 1)
+                    {
+                        productvalue = data[i, j];
+                    }
+                    else if (j == 2)
+                    {
+                        userRating = data[i, j];
+                        var userdata = new double[,]
+                        {
+                            {productvalue,userRating}
+                        };
+                        if (dictionary.ContainsKey(key))
+                        {
+                            var data2 = new double[,] { };
+                            data2 = dictionary[key];
+                            int rowLengthdata2 = data2.GetLength(0) - 1;
+                            int colLenghtdata2 = 2;
+                            int rowLengthdata3 = rowLengthdata2 + 1;
+                            int newRow2Ddata3 = data2.GetLength(0) + 1;
+                            var data3 = new double[newRow2Ddata3, colLenghtdata2];
+
+                            for (int r = 0; r <= rowLengthdata2; r++)
+                            {
+                                for (int c = 0; c < colLenghtdata2; c++)
+                                {
+                                    data3[r, c] = data2[r, c];
+                                }
+
+                            }
+                            data3[rowLengthdata3, 0] = productvalue;
+                            data3[rowLengthdata3, 1] = userRating;
+                            dictionary[key] = data3;
+
+                        }
+                        else { dictionary.Add(key, userdata); }
+
+                    }
+
+                }
+            }
+            return dictionary; 
+        }
+    }
+}
--- UserItem/Program.cs
+++ UserItem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UserItem.Data;
 
 namespace UserItem
 {
@@ -37,76 +38,15 @@ namespace UserItem
 {6,101,3.0},
 {7,102,4.5},
 {7,105,1.0}};
-            var dictionary = new Dictionary<double, double[,]>();
-            int rowLength = data.GetLength(0);
-            int colLenght = data.GetLength(1);
-            bool checkeNewRow = false;
-
-            for (int i = 0; i < rowLength; i++)
-            {
-                double key = -1;
-                double productvalue = -1;
-                double userRating = -1;
-                checkeNewRow = false;
-
-                for (int j = 0; j < colLenght; j++)
-                {
-                    Console.WriteLine(data[i,j]);
-                    if(!checkeNewRow)
-                    {
-                        key = data[i, j];
-                        checkeNewRow = true;
-
-                    }
-
-                    else if(j == 1 ){
-                        productvalue = data[i, j];
-                    }
-                    else if (j == 2)
-                    {
-                        userRating = data[i, j];
-                        var userdata = new double[,]
-                        {
-                            {productvalue,userRating}
-                        };
-                        //Console.Write(userdata[0,0]+" , "+userdata[0,1]);
-                        if (dictionary.ContainsKey(key))
-                        {
-                            var data2 = new double[,] { };
-                            data2 = dictionary[key];
-                            int rowLengthdata2 = data2.GetLength(0)-1;
-                            //if(rowLengthdata2 == 0)
-                            //{
-                            //    rowLengthdata2 = 1;
-                            //}
-                            int colLenghtdata2 = 2;
-                            int rowLengthdata3 = rowLengthdata2 + 1;
-                            int newRow2Ddata3 = data2.GetLength(0) + 1;
-
-                             var data3 = new double[newRow2Ddata3, colLenghtdata2];
-                            //var data3 = new double[2, 2];
-
-                            for (int r = 0; r <= rowLengthdata2; r++)
-                            {
-                                for (int c = 0; c < colLenghtdata2; c++)
-                                {
-                                    data3[r,c]= data2[r, c];
-                                }
-
-                            }
-                            //data3 = data2;
-                            data3[rowLengthdata3, 0] = productvalue;
-                            data3[rowLengthdata3, 1] = userRating;
-                            dictionary[key] = data3;
-
-                        }
-                        else { dictionary.Add(key, userdata); }
-                      
-                    }
-                    
-                }
-            }
+            Dictionary<double, double[,]> dictionary = getData(data);
             Console.ReadLine();
         }
+        public static Dictionary<double, double[,]> getData(double[,] data)
+        {
+            FileReader r = new FileReader();
+            Dictionary<double, double[,]> dictionary = r.GetData(data);
+            return dictionary;
+        }
+
     }
 }
